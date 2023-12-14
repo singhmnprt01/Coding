27239acfdfee6a388ae1a737c73ab001fc04403a
Manpreet Singh <62533486+singhmnprt01@users.noreply.github.com>
2023-12-14 20:49:56 +0530

    Add files via upload

--- LeftMostRepeatingCharacter_14122023.py
+++ LeftMostRepeatingCharacter_14122023.py
@@ -1,4 +1,6 @@
-def leftmost(mystr):
+
+import sys
+def leftmost_method1(mystr):
     
     helper = [0]*256
     
@@ -9,7 +11,26 @@ def leftmost(mystr):
         if helper[ord(mystr[i])] >1:
             return i
     return -1
+
+def leftmost_method2(mystr):
+    helper = [-1]*256
+    result = sys.maxsize
+    for i in range(len(mystr)):
+        ord_value =  ord(mystr[i])
+        
+        if helper[ord_value]==-1:
+            helper[ord_value]=i
+            
+        else:
+            result = min(result, helper[ord_value])
+    
+    if result == sys.maxsize:
+        return -1
+    else:
+        return result
+
         
 if __name__ == '__main__':
     mystr = input("Enter the stirng: ")
-    print(leftmost(mystr))
+    print(leftmost_method1(mystr))
+    print(leftmost_method2(mystr))
